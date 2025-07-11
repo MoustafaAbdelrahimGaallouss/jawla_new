74688d5f27d462c1af59e3ad1c29bac4929b5050
Moustafa AbdEl-Rahim Ga3llouss <141064242+MoustafaAbdelrahimGaallouss@users.noreply.github.com>
2025-07-11 19:14:36 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:get/get.dart';
 import 'package:jawla/core/constants/routes.dart';
 import 'package:jawla/core/constants/theme.dart';
